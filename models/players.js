419181863ad008f56ed368d0eb1d87985769e5be
Diya Solanki <103099073+Diyasolanki@users.noreply.github.com>
2025-01-08 21:12:20 +0530

    Update players.js

--- models/players.js
+++ models/players.js
@@ -1,43 +1,13 @@
 const mongoose = require('mongoose')
 
-// const playerScehma = new mongoose.Schema( {
-//     name : {
-//         type : 'string',
-//         required : true,
-//         minlength : 2,
-//     },
-//     country : { 
-//         type : 'string',
-//         required : true,
-//         minlength : 3
-//     },
-
-//     sport: {
-//         type : 'string',
-//         required : true,
-//         minlength : 5,
-//     },
-
-//     medal : {
-//         type : 'string',
-//         required : true,
-//         minlength : 4,
-//     },
-
-//     year :{
-//         type : 'Number',
-//         required : true,
-//         maxlength : 4
-//     }   
-// })
 const playerScehma = new mongoose.Schema({
     name: {
-        type: String, // Correct
+        type: String,
         required: true,
         minlength: 2,
     },
     country: { 
-        type: String, // Correct
+        type: String, 
         required: true,
         minlength: 3,
     },
@@ -52,9 +22,9 @@ const playerScehma = new mongoose.Schema({
         minlength: 4,
     },
     year: {
-        type: Number, // Correct
+        type: Number, 
         required: true,
-        maxlength: 4, // Note: This should probably be `max` instead of `maxlength`
+        maxlength: 4,     
     }
 });
 
